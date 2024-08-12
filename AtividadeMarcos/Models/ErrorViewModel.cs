3f9bc5e062f14c92f3a5089d4d285b54101ba7a0
= <ryannogueiranogueira0@gmail.com>
2024-08-12 14:41:18 -0300

    Versão inicial

---
+++ AtividadeMarcos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AtividadeMarcos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
